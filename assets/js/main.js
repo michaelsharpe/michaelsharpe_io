4de41553d0cdf71fcea6cab3bc2eb8ebf1388c8a
michaelsharpe <m.sharpe@project89.org>
2014-04-07 15:52:35 -0400

    Social links in svg working.  Added favicon.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,4 +22,9 @@ $(document).ready(function(){
       $(".social-icon").css("margin", "0 1rem");
     }
   })
+
+  // Make icons into links
+  $(".social-icons li").on("click", function() {
+    console.log("test")
+  });
 });
